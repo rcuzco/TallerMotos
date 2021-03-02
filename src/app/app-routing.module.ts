9c982d21f354f4058271baf06cf3a37bd459e6f3
rcuzco <ronald.cuzco@gmail.com>
2021-03-02 13:52:17 +0100

    primera subida

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClientesComponent } from './pages/clientes/clientes.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { PagesComponent } from './pages/pages/pages.component';
 
-const routes: Routes = [];
+const routes: Routes =
+  [
+    //estas rutas se mostrarán en el router-outlet de appComponent
+    {
+      path: '',
+      component: PagesComponent,
+      children: //estas rutas las mostrará en el router-outlet de PagesComponent
+        [
+          { path: 'dashboard', component: DashboardComponent },
+          { path: 'clientes', component: ClientesComponent },
+          { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+        ]
+    },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+
+
+    { path: '**', component: NopagefoundComponent }//cualquier ruta q no esté contemplada, q me lleve a no page found
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { ClientesComponent } from './pages/clientes/clientes.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { PagesComponent } from './pages/pages/pages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HeaderComponent,
+    BreadcrumbsComponent,
+    NopagefoundComponent,
+    FooterComponent,
+    ClientesComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    PagesComponent
   ],
   imports: [
     BrowserModule,
